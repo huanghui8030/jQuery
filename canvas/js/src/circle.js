caacda8ced591b20f9c71e30a12fe8d5c6eb4e26
黄卉 <326028030@qq.com>
2017-01-05 10:26:53 +0800

    低于50%为黄色，高于用绿色，添加delay参数，设置时间。

--- canvas/js/src/circle.js
+++ canvas/js/src/circle.js
@@ -9,9 +9,14 @@ var pie = {
             var before = opts.onBefore || noop;
             var after = opts.onAfter || noop;
             before(ctx);
-            ctx.fillStyle = opts.color || '#f9684b';
+            if(opts.percent<0.5){
+                ctx.fillStyle = opts.color || '#f9684b';
+            }else{
+                ctx.fillStyle = opts.color || '#48B325';
+            }
+            
             var step = opts.step || 1;
-            var delay = opts.delay || 10;
+            var delay = opts.delay || 3;
             var i = 0, rage = 360 * (opts.percent || 0);
             var sRage = -Math.PI * 0.5;
             var djs = function(){
@@ -33,11 +38,14 @@ var pie = {
 var p=0;
 var len=$(".jqm-round-wrap").length;
 for(p=0;p<len;p++){
+    //获取百分比
 	var k=$("#jqm-round-sector"+p).next(".jqm-round-circle").find("p").html();
 		k=percentTofloat(k);
+    //设置pie中opts的值
 	pie.run({
 		id:"jqm-round-sector"+p,
 		percent: k,
+        delay:parseFloat($("#jqm-round-sector"+p).attr('delay')),
 		onBefore:function(ctx){
 			ctx.fillStyle = '#ebebeb';
 			ctx.beginPath();
@@ -47,6 +55,7 @@ for(p=0;p<len;p++){
 		}
 	});
 }
+//处理百分数，转为小数。
 function percentTofloat(km){
 		if(km.substring(km.length-1,km.length)=="%"){
 			km = km.substring(0,km.length-1);

fa6520445ed1551c1876b7cf756868184c488083
黄卉 <326028030@qq.com>
2017-05-26 16:34:24 +0800

    添加各种插件用法
    1、仿百度图片浏览功能；
    2、瀑布流插件；
    3、bootstrap多选框插件

--- 仿百度图片浏览功能/js/data.js
+++ 仿百度图片浏览功能/js/data.js
@@ -1,102 +1,27 @@
 var imgData = [
 	{
 		src: 'images/img01.jpg',
-		title: 'a1.jpg',
-		name: '火影忍着1'
+		title: '图1',
+		name: '图1'
 	},
 	{
 		src: 'images/img02.jpEg',
-		title: 'a2.jpg',
-		name: '火影忍着3'
+		title: '图2',
+		name: '图2'
 	},
 	{
 		src: 'images/img03.jpg',
 		title: 'a3.jpg',
-		name: '火影忍着3'
+		name: '图3'
 	},
 	{
-		src: 'images/a4.jpg',
-		title: 'a4.jpg',
-		name: '火影忍着4'
+		src: 'images/img04.jpg',
+		title: '火影忍着4',
+		name: '图4'
 	},
 	{
-		src: 'images/a5.jpg',
-		title: 'a5.jpg',
-		name: '火影忍着5'
-	},
-	{
-		src: 'images/a6.jpg',
-		title: 'a6.jpg',
-		name: '火影忍着6'
-	},
-	{
-		src: 'images/a7.jpg',
-		title: 'a7.jpg',
-		name: '火影忍着7'
-	},
-	{
-		src: 'images/a8.jpg',
-		title: 'a8.jpg',
-		name: '火影忍着8'
-	},
-	{
-		src: 'images/a9.jpg',
-		title: 'a9.jpg',
-		name: '火影忍着9'
-	},
-	{
-		src: 'images/a10.jpg',
-		title: 'a10.jpg',
-		name: '火影忍着10'
-	},
-	{
-		src: 'images/a11.jpg',
-		title: 'a11.jpg',
-		name: '火影忍着11'
-	},
-	{
-		src: 'images/a12.jpg',
-		title: 'a12.jpg',
-		name: '火影忍着12'
-	},
-	{
-		src: 'images/a13.jpg',
-		title: 'a13.jpg',
-		name: '火影忍着13'
-	},
-	{
-		src: 'images/a14.jpg',
-		title: 'a14.jpg',
-		name: '火影忍着14'
-	},
-	{
-		src: 'images/a15.jpg',
-		title: 'a15.jpg',
-		name: '火影忍着15'
-	},
-	{
-		src: 'images/a16.jpg',
-		title: 'a16.jpg',
-		name: '火影忍着16'
-	},
-	{
-		src: 'images/a17.jpg',
-		title: 'a17.jpg',
-		name: '火影忍着17'
-	},
-	{
-		src: 'images/a18.jpg',
-		title: 'a18.jpg',
-		name: '火影忍着18'
-	},
-	{
-		src: 'images/a19.jpg',
-		title: 'a19.jpg',
-		name: '火影忍着19'
-	},
-	{
-		src: 'images/a20.jpg',
-		title: 'a20.jpg',
-		name: '火影忍着20'
+		src: 'images/img05.jpeg',
+		title: '图5',
+		name: '图6'
 	}
 ];
